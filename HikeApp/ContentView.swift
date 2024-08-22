e692297d286231a410df1b055a90e712858fa1d2
Alice <114536079+AliceC0874203@users.noreply.github.com>
2024-08-21 20:31:51 -0400

    Initial Commit

---
+++ HikeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HikeApp
+//
+//  Created by Alice’z Poy on 2024-08-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
